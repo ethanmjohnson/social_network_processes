505f208f0410bbab9ee29408009ebae7bc436c87
ethanmjohnson <ethan_m_johnson@me.com>
2024-11-18 11:42:14 +1030

    updated petri net visualisation

--- code/analysis.py
+++ code/analysis.py
@@ -1,9 +1,11 @@
 # a module containing functions for use with analysing event logs
 
 from pm4py.objects.log.importer.xes import importer as xes_importer
-from pm4py.objects.log.obj import EventLog
+from pm4py.objects.log.obj import EventLog, Trace
 import os
 import numpy as np
+import pm4py
+import random
 
 
 def load_log(file_path):
@@ -12,6 +14,14 @@ def load_log(file_path):
     parameters = {variant.value.Parameters.TIMESTAMP_SORT: True}
     log = xes_importer.apply(file_path, variant=variant, parameters=parameters)
     return log
+
+def discover_petri(eventlog, visualise = False):
+    print('discovering...')
+    net, im, fm = pm4py.discover_petri_net_inductive(eventlog)
+    if visualise is True:
+        print('visualising...')
+        pm4py.view_petri_net(net, im, fm)
+    return (net, im, fm)
 
 
 def calculate_interarrival_times(eventlog: EventLog) -> list:
@@ -34,5 +44,61 @@ def calculate_interarrival_times_folder(folder_path):
             if os.path.isfile(file_path):
                 log = load_log(file_path)
                 times = calculate_interarrival_times(log)
-                average_intertime.append(np.sum(times)/len(log))
+                average_intertime.append(np.mean(times))
     return average_intertime
+
+def discover_model(log: EventLog, view_log = False):
+    print('discovering...')
+    net, im, fm = pm4py.discover_petri_net_inductive(log)
+
+    if view_log:
+        print('visualising...')
+        pm4py.view_petri_net(net, im, fm, format='png')
+    #pm4py.save_vis_petri_net(net, im, fm, "/Users/ethanjohnson/Desktop/mphil-project/figs/higgs_petri_postlog.png")
+
+    return net, im, fm
+
+
+def filter_log_activity_count(event_count, log):
+    # this function removes activities that are seen fewer than the event_count in the log
+
+    eventlist = []
+
+    for trace in log:
+        for event in trace:
+            eventlist.append(event['concept:name'])
+
+    unique_elements, counts = np.unique(eventlist, return_counts=True)
+
+    combined_counts = list(zip(unique_elements, counts))
+
+    filtered_events = []
+    for element in combined_counts:
+        if element[1] > event_count:
+            filtered_events.append(element[0])
+
+    filtered_log = EventLog()
+
+    for trace in log:
+        filtered_trace = Trace()
+        i=0
+        for event in trace:
+            if event['concept:name'] in filtered_events:
+                filtered_trace.insert(i, event)
+                i+=1
+        filtered_log.append(filtered_trace)
+    
+    extra_filtered_log = pm4py.filter_case_size(filtered_log, 2, 2**50)
+    
+    return extra_filtered_log
+
+
+def filter_log_size(log_length, log):
+    # this function randomly reduces the number of traces in log down to the size of log_length
+
+    sample = random.sample(list(range(len(log))), log_length)
+    new_log = EventLog()
+    for number in sample:
+        new_log.append(log[number])
+
+    return new_log
